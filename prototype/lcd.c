c88806e0326f5cd2e95f87bc4b211e50567faec8
alex griffin <alexander.griffin99@hotmail.com>
2017-11-15 09:43:41 -0500

    commented somethings

--- prototype/lcd.c
+++ prototype/lcd.c
@@ -1,61 +1,80 @@
 #ifndef _PROTOTYPE_LCD_
 #define _PROTOTYPE_LCD_
 
+// LCD button combonations for a single button being pressed 
+// 3 bit bin value converted to dec
 #define LEFT_BUTTON 1
 #define CENTER_BUTTON 2
 #define RIGHT_BUTTON 4
 
+// the number of auton programs the lcd should run through
 #define AUTON_NUMBER 4
 
+
+// the names of the auton programs
 const string LCD_STRINGS[AUTON_NUMBER] = { "RIGHT 22", "LEFT 22",
                                            "RIGHT 7",  "LEFT 7"
                                          };
 
 
 
-// forward declaration
-typedef struct lcdPane;
 
+// requires: null
+// modifies: null
+// affects:  waits until an lcd button is pressed
 void waitForPress() {
+    
  while(nLCDButtons == 0) {}
  wait1Msec(20);
+
+    
 }
 
+// requires: null
+// modifies: null
+// affects:  waits until an lcd button is released
 void waitForRelease() {
+ 
  while(nLCDButtons != 0) {}
  wait1Msec(20);
+ 
 }
 
+
+// requires: null
+// modifies: null
+// affects:  clears the lcd
 void clearLCD() {
   clearLCDLine(0);
   clearLCDLine(1);
 }
 
+
 typedef struct {
 
-  int data;
-  string lines[2];
+  int data;        // may be able to be removed consider refactoring
+  string lines[2]; // the data to be printed to the lcd
 
 } lcdPane;
 
 typedef struct {
 
-  int count;
-  int active;
+  int active; // the active pane (index of panes array)
 
 } LCD;
 
-LCD lcd;
-lcdPane panes[AUTON_NUMBER];
+LCD lcd;                     // the global lcd
+lcdPane panes[AUTON_NUMBER]; // the panes of the lcd 
 
 void lcdInit() {
   for(int i = 0; i < AUTON_NUMBER; i++) {
-    panes[i].data     = i;
-    panes[i].lines[0] = LCD_STRINGS[i];
-    panes[i].lines[1] = "<     Enter    >";
-
-
+  
+    panes[i].data     = i;                  // consider refactoring
+    panes[i].lines[0] = LCD_STRINGS[i];     // sets the auton name 
+    panes[i].lines[1] = "<     Enter    >"; // can be changed
+    
   }
+  
   lcd.active = 0;
   clearLCD();
 
@@ -66,36 +85,41 @@ void lcdInit() {
 
 void selectAuton() {
 
-  while(nLCDButtons != CENTER_BUTTON) {
+  while(nLCDButtons != CENTER_BUTTON) { 
+    // continue until the user selects the auton
+    
     displayLCDCenteredString(0, panes[lcd.active].lines[0]);
     displayLCDCenteredString(1, panes[lcd.active].lines[1]);
+    
     waitForPress();
 
     if(nLCDButtons == LEFT_BUTTON) {
       waitForRelease();
-      lcd.active = lcd.active == 0 ? AUTON_NUMBER - 1 : lcd.active -= 1;
+      lcd.active = lcd.active == 0 ? AUTON_NUMBER - 1 : lcd.active -= 1; 
+      // wraps around if the active is 0
 
     } else if(nLCDButtons == RIGHT_BUTTON) {
       waitForRelease();
       lcd.active = lcd.active == AUTON_NUMBER - 1 ? 0 : lcd.active += 1;
-
+      // wraps to prevent bad index of array
     }
   }
-
-
-
 }
 
 
 void runAuton() {
   for(int i = 0; i < AUTON_NUMBER; i++) {
-    if(i == panes[lcd.active].data == i) {
+    if(i == panes[lcd.active].data) {
+      
       displayLCDCenteredString(0, LCD_STRINGS[i]);
       displayLCDCenteredString(1, "is running");
 
     }
   }
 
+  // unfortunately robotC does not allow function pointers 
+  // so we are left with this mess 
+  
   if(panes[lcd.active].data == 0) {
     auton22ptRight();
   } else if(panes[lcd.active].data == 1) {
