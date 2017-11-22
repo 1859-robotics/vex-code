bcce33e4c34115275af24a1db8d3fbccccc2692e
Alexander Griffin <alexander.griffin99@hotmail.com>
2017-11-22 17:42:54 -0500

    _

--- prototype/pid.c
+++ prototype/pid.c
@@ -6,64 +6,60 @@
 
 
 typedef struct {
-    
-    int     request;
-    int     current;
-    
-    int     error;
-    int     prevError;
-    
-    int     P;
-    int     I;
-    int     D;
-    
-    int     kP;
-    int     kI;
-    int     kD;
-    
-    tMotor  motor;
-    tSensor sensor;
-    
+
+  int request;
+  int current;
+
+  int error;
+  int prevError;
+
+  int P;
+  int I;
+  int D;
+
+  int kP;
+  int kI;
+  int kD;
+
+  int iLim;
+
+  tMotor  motor;
+  tSensor sensor;
+
 } PIDcontrol;
 
 
-void pidInit(PIDcontrol *pid, int request, int current, tMotor m, tSensor s, int KP = 0, int KI = 0, int KD = 0) {
-    
-    *pid.request = request;
-    *pid.current = current;
-    *pid.error = current - request;
-    *pid.prevError = *pid.error;
-    
-    
-    *pid.kP = KP;
-    *pid.Ki = KI;
-    *pid.Kd = KD;
-    
-    *pid.motor = m;
-    *pid.sensor = s;
-    
+void pidInit(PIDcontrol &pid,
+             int request, int current,
+             tMotor m, tSensor s,
+             int KP = 0, int KI = 0, int KD = 0) {
+
+  pid.request = request;
+  pid.current = current;
+  pid.error = current - request;
+  pid.prevError = pid.error;
+
+
+  pid.kP = KP;
+  pid.Ki = KI;
+  pid.Kd = KD;
+
+  pid.motor = m;
+  pid.sensor = s;
+
 }
 
 void SetRequest(PIDcontrol *pid, int request) {
-    *pid.request = request;
+  *pid.request = request;
 }
 
 int pid(PIDcontrol *pid) {
-    
-    
-    
-    
-    return 0;
+
+
+
+
+  return 0;
 }
 
 
-/*
-
-24 - english
-28 - math
-30 - reading
-24 - science
-27 - composite 
-
-*/
 #endif
