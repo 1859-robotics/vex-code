ed40b75d46f123d18e70516299bf31947dd759df
Griffin <alexander.griffin@stu.jessamine.kyschools.us>
2018-02-02 20:50:42 -0500

    _

--- prototype/auton.c
+++ prototype/auton.c
@@ -52,8 +52,6 @@ void autonRight22() {
   writeDebugStream("end \n")
 }
 
-
-
 void autonRight9() {
   resetEncoders();
   core(100, false);
@@ -89,10 +87,10 @@ void autonRight9() {
 void autonLeft9() {
   resetEncoders();
   core(100, false);
-  manipulate(1, false);
-  swerveRightGyro(-48);
+  manipulate(1, true);
+  swerveRightGyro(48);
   moveCenter(850, 127, true);
-  moveCenter(270, 40, true);
+  moveCenter(300, 40, true);
   manipulate(-1, true); // pick up MB
   moveCenter(30, 40, true);
   drive.canMove = false;
@@ -116,4 +114,39 @@ void autonLeft9() {
 
 }
 
+void autonLeft22() {
+  resetEncoders();
+  core(100, false);
+  manipulate(1, false);
+  swerveRightGyro(48);
+  moveCenter(850, 127, true);
+  moveCenter(270, 40, true);
+  manipulate(-1, true); // pick up MB
+  moveCenter(30, 40, true);
+  drive.canMove = false;
+  core(-100, true);
+  claw(500, true); // put cone on base
+  flip(650, -1, true);
+  claw(-750, true); // pick up next cone
+  core(100, true);
+  flip(650, 1, true);
+  drive.canMove = true;
+  moveCenter(720, -127, false);
+  claw(-300, true); // pick up next cone
+  while(!drive.canMove) {}
+  core(-100, true);
+  claw(500, true); // put next cone on base
+  core(200, false);
+  swerveLeftGyro(-48);
+  moveCenter(300, -127, true);
+  manipulator.canMove = false
+  turn(90);
+  manipulator.canMove = true
+  manipulate(1, false);
+  moveCenter(600, 100, true);
+  moveCenter(500, -127, true);
+  manipulate(-1, true);
+
+  writeDebugStream("end \n")
+}
 #endif
